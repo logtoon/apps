420df42b39ea17ea66422d3a71213a3f2dd7cb0b
Ravi <Ravi>
2017-03-16 14:28:23 +0530

    change 4

--- index.js
+++ index.js
@@ -6,67 +6,68 @@ const request = require('request')
 
 const app = express()
 
-app.set('port',(process.env.PORT) || 5000)
+app.set('port', (process.env.PORT || 5000))
 
-//processing data here
-app.use(bodyParser.urlencoded({extended:false}))
+// Allows us to process the data
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-//routes
-app.get('/', function(req, res){
-	res.send("Hello ! Its a Chatbot")	
-	
+// ROUTES
+
+app.get('/', function(req, res) {
+	res.send("Hi I am a chatbot")
 })
 
-let token ="EAASyePdFYgABAPLRnfdLWyarO0yfOQTqzLY476AsZAO5Edinc4mQ92ghelXTghO07Q0yK3tMfldkZC2W75gP6KX83sZB9yH9WWdHNgHsZA5ZCP4NcsjojX7jRTk1xrAR3Lzu4X2NoenxHSyZC9GXTrMGPWJV00Xizm0Gc4jM0fIQZDZD"
+let token = "EAASyePdFYgABAI83GTUqjyKpYtm40YePPdFlWZBbONmo0ohMn5mZCeFP8MW3FTsGXmR6KZB0aGZA74Xqf7jk48oog6lpTVW5QfS52WyBZBZCYIBBjS25phgpEWd7qrZBX09QptsgvC2lVqCSxKRLRhlHQF7DPeIbJMxMDuAX1JoEwZDZD"
 
-//facebook functions
+// Facebook 
 
-app.get('/webhook/', function(req, res){
-	
-	if(req.query['hub.verify_token'] === "tokencheck123"){
-			res.send(req.query['hub.challenge'])
-		}
-		res.send("Wrong token")
+app.get('/webhook/', function(req, res) {
+	if (req.query['hub.verify_token'] === "tokencheck123") {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send("Wrong token")
 })
 
-app.post('/webhook/', function(req, res){
+app.post('/webhook/', function(req, res) {
 	let messaging_events = req.body.entry[0].messaging
-	
-	for(let i = 0; i<messaging_events.length; i++){
+	for (let i = 0; i < messaging_events.length; i++) {
 		let event = messaging_events[i]
-		let sender = event.sender,id
-		if(event.message && event.message.text){
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
 			let text = event.message.text
-			sendText(sender, "Text echo: "+text.substring(0,100))
-			}
+			sendText(sender, "Text echo: " + text.substring(0, 100))
 		}
-		res.sendStatus(200)
-		
-	
-	})
-	
-function sendText(sender, text){
-	
-	let messageData={text: text}
-	request({
-		url:"https://graph.facebook.com/v2.6/me/messages",
-		qs:{access_token:token},
-		method:"POST",
-		json:{
-			recipient:{id:sender},
-			message:messageData,
-			}
-		}, function(error, response, body){
-				if(error){
-					console.log("sending error")
-					}
-				else if(response.body.error){
-					console.log("response body error")
-					}	
-			})
-	}	
+	}
+	res.sendStatus(200)
+})
 
-app.listen(app.get('port'), function(){
-		console.log("running: port")
+function sendText(sender, text) {
+	let messageData = {text: text}
+	request({
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs : {access_token: token},
+		method: "POST",
+		json: {
+			recipient: {id: sender},
+			message : messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log("sending error")
+		} else if (response.body.error) {
+			console.log("response body error")
+		}
 	})
+}
+
+app.listen(app.get('port'), function() {
+	console.log("running: port")
+})
+
+
+
+
+
+
+
